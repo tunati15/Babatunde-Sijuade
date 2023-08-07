75b07116c73d38ac1b1edd790735c5fe990d1ff0
Masters thesis project <118276088+tunati15@users.noreply.github.com>
2023-08-07 18:05:54 +0100

    update

--- Dockerfile
+++ Dockerfile
@@ -1,19 +1,24 @@
+# Specifying the base Image
 FROM node:14-alpine
 
 # Create app directory
+# Setting work directory in the container
 WORKDIR /app
 
 # Install app dependencies
-# A wildcard is used to ensure both package.json AND package-lock.json are copied
 # where available (npm@5+)
+# A wildcard is used to ensure both package.json AND package-lock.json are copied
+#  Copying the package.json and package-lock.json to the container
 COPY package*.json ./
 
-RUN npm install
+
 # If you are building your code for production
 # RUN npm ci --omit=dev
+RUN npm install
 
 # Bundle app source
 COPY . /app 
 
+# Expose port 
 EXPOSE 80
 CMD [ "node", "server.js" ]
--- server.js
+++ server.js
@@ -1,93 +1,21 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const Product = require('./models/productModel')
-const app = express()
+"use strict";
 
-app.use(express.json())
-app.use(express.urlencoded({extended: false}))
+const express = require("express");
 
-//routes
+// Constants
+const PORT = 3000;
+const HOST = "0.0.0.0";
 
-app.get('/', (req, res) => {
-    res.send('Hello NODE API')
-})
+// App
+const app = express();
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 
-app.get('/blog', (req, res) => {
-    res.send('Hello Blog, My name is Devtamin')
-})
+app.get("/planning-poker", (req, res) => {
+  res.send("Future Planning Poker URL");
+});
 
-app.get('/products', async(req, res) => {
-    try {
-        const products = await Product.find({});
-        res.status(200).json(products);
-    } catch (error) {
-        res.status(500).json({message: error.message})
-    }
-})
-
-app.get('/products/:id', async(req, res) =>{
-    try {
-        const {id} = req.params;
-        const product = await Product.findById(id);
-        res.status(200).json(product);
-    } catch (error) {
-        res.status(500).json({message: error.message})
-    }
-})
-
-
-app.post('/products', async(req, res) => {
-    try {
-        const product = await Product.create(req.body)
-        res.status(200).json(product);
-        
-    } catch (error) {
-        console.log(error.message);
-        res.status(500).json({message: error.message})
-    }
-})
-
-// update a product
-app.put('/products/:id', async(req, res) => {
-    try {
-        const {id} = req.params;
-        const product = await Product.findByIdAndUpdate(id, req.body);
-        // we cannot find any product in database
-        if(!product){
-            return res.status(404).json({message: `cannot find any product with ID ${id}`})
-        }
-        const updatedProduct = await Product.findById(id);
-        res.status(200).json(updatedProduct);
-        
-    } catch (error) {
-        res.status(500).json({message: error.message})
-    }
-})
-
-// delete a product
-
-app.delete('/products/:id', async(req, res) =>{
-    try {
-        const {id} = req.params;
-        const product = await Product.findByIdAndDelete(id);
-        if(!product){
-            return res.status(404).json({message: `cannot find any product with ID ${id}`})
-        }
-        res.status(200).json(product);
-        
-    } catch (error) {
-        res.status(500).json({message: error.message})
-    }
-})
-
-mongoose.set("strictQuery", false)
-mongoose.
-connect('mongodb+srv://admin:12345678Admin@devtaminapi.zpncstm.mongodb.net/Node-API?retryWrites=true&w=majority')
-.then(() => {
-    console.log('connected to MongoDB')
-    app.listen(80, ()=> {
-        console.log(`Node API app is running on port 80`)
-    });
-}).catch((error) => {
-    console.log(error)
-})
+app.listen(PORT, HOST, () => {
+  console.log(`Running on http://${HOST}:${PORT}`);
+});
